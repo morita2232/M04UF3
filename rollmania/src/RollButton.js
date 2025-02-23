b231cae58225fca3d15a60c93baa1d4548d46f6b
ENTI <sebastian.mora@enti.cat>
2025-02-23 17:37:19 +0100

    Ejercicio Propiedades y Ganchos

--- rollmania/src/RollButton.js
+++ rollmania/src/RollButton.js
@@ -1,11 +1,44 @@
 import './RollButton.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function RollButton(props) {
 
+let [countdown, setCountdown] = useState(null);
+
+useEffect(function() {
+	if (countdown === 0){
+		let timeout = setTimeout(function () {
+			props.roll_func();
+			setCountdown(null);
+		}, 1000);
+		return function() {clearTimeout(timeout);};
+	}
+
+	if (countdown > 0) {
+		let interval = setInterval(function() {
+			setCountdown(function(prev) {
+			return prev - 1;
+		});
+	}, 1000);
+		return function() {clearInterval(interval); };
+	}
+}, [countdown, props.roll_func]);
+
+function handleClick() {
+	setCountdown(5);
+}
 	return (
-	<button className="Roll" onClick={props.roll_func} >
-		<p>ROLL</p>
+	<button 
+		className="Roll" 
+			onClick={handleClick}
+				>
+		<p>
+			{countdown === null
+				? "ROLL"
+				: countdown > 0
+				? countdown
+				: "ROLL"}
+		</p>
 	</button>
 	);
 }
